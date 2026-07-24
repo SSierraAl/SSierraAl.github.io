f6fd522b933287a4e112707a03aba87c6546c238
Sebastián Sierra <42323058+SebasSA1@users.noreply.github.com>
2026-07-24 17:26:26 +0200

    colors

--- js/site-config.js
+++ js/site-config.js
@@ -13,7 +13,7 @@ var siteConfigData = {
 
   "COLORES": {
     "nota": "Variables CSS para personalizar colores. Usa códigos hex como #ad1f4a",
-    "primary_color": "#ad1f4a",
+    "primary_color": "#3b0f28",
     "primary_dark": "#6a0828",
     "primary_light": "#f2c5e0",
     "accent_color": "#d43790",
